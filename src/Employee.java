6664e7ef598ce79fa949ccc58a18cb94134c6115
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-21 14:22:48 +0530

    Added Attendance

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,15 @@
-
+import java.lang.Math;
 public class Employee {
+	public boolean checkAttendance() {
+		double attendance = Math.random();
+		if(attendance < 0.5) {
+			return false;
+		}
+		return true;
+	}
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Computation");
+		Employee e1 = new Employee();
+		System.out.println("Employee Attendance: "+e1.checkAttendance());
 	}
 }
