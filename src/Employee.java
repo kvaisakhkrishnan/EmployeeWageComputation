06fcfaf4ed4d6baa0c0727c99f9c81b72fed159f
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-21 14:44:15 +0530

    Daily Wage Added

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,5 @@
 import java.lang.Math;
+import java.util.Scanner;
 public class Employee {
 	public boolean checkAttendance() {
 		double attendance = Math.random();
@@ -7,9 +8,26 @@ public class Employee {
 		}
 		return true;
 	}
+	public int fullDayWage(int hoursWorked) {
+		if(hoursWorked > 0 && hoursWorked <= 8) {
+			return hoursWorked * 20;
+		}
+		else {
+			System.out.println("Invalid Hours Worked Value");
+			return 0;
+		}
+	}
 	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to Employee Wage Computation");
 		Employee e1 = new Employee();
-		System.out.println("Employee Attendance: "+e1.checkAttendance());
+		boolean checkAttendance = e1.checkAttendance();
+		System.out.println("Employee Attendance: "+checkAttendance);
+		if(checkAttendance == true) {
+			System.out.println("Enter no of hours worked");
+			int noOfHours = scanner.nextInt();
+			System.out.println("Daily Wage is : "+ e1.fullDayWage(noOfHours));
+		}
+		
 	}
 }
