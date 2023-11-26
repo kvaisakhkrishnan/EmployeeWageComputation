d61132e3a7786f0f6d25f1b3945220687da7bd0b
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-26 17:49:31 +0530

    Multiple Company Added

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,45 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main {
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
+		ArrayList<Company> companyList = new ArrayList<Company>();
 		System.out.println("Welcome to Employee Wage Computation");
-		System.out.println("Enter Full Time Working Wage: ");
-		int fullTimeWage = scanner.nextInt();
-		System.out.println("Enter Part Time Working Wage: ");
-		int partTimeWage = scanner.nextInt();
-		System.out.println("Enter No of Full Time Working Hours: ");
-		int fullTimeHours = scanner.nextInt();
-		System.out.println("Enter No of Part Time Working Hours: ");
-		int partTimeHours = scanner.nextInt();
-		System.out.println("Enter No of Working Days: ");
-		int workingdays = scanner.nextInt();
-		System.out.println("Enter No of Working Hours Total: ");
-		int workinghours = scanner.nextInt();
-		Company company = new Company(fullTimeWage, partTimeWage, fullTimeHours, partTimeHours, workingdays, workinghours);
-		Employee e1 = new Employee();
-		company.addEmployee(e1);
-		Employee e2 = new Employee();
-		company.addEmployee(e2);
-		company.wageComputation(e1);
-		company.totalWage();
+		System.out.println("1.Add Company\n");
+		
+		while(true) {
+			System.out.println("1. Add Company\n2. Employee Wage\n3. Exit");
+			int option = scanner.nextInt();
+			if(option == 1) {
+				System.out.println("Enter Full Time Working Wage: ");
+				int fullTimeWage = scanner.nextInt();
+				System.out.println("Enter Part Time Working Wage: ");
+				int partTimeWage = scanner.nextInt();
+				System.out.println("Enter No of Full Time Working Hours: ");
+				int fullTimeHours = scanner.nextInt();
+				System.out.println("Enter No of Part Time Working Hours: ");
+				int partTimeHours = scanner.nextInt();
+				System.out.println("Enter No of Working Days: ");
+				int workingdays = scanner.nextInt();
+				System.out.println("Enter No of Working Hours Total: ");
+				int workinghours = scanner.nextInt();
+				Company company = new Company(fullTimeWage, partTimeWage, fullTimeHours, partTimeHours, workingdays, workinghours);
+				Employee e1 = new Employee();
+				Employee e2 = new Employee();
+				company.addEmployee(e1);
+				company.addEmployee(e2);
+				companyList.add(company);
+				System.out.println("Added Company and employee");
+			}
+			else if(option == 2) {
+				for(int i = 0; i < companyList.size(); i++) {
+					System.out.println("-------------------------------------");
+					System.out.println("Company "+i);
+					companyList.get(i).totalWage();
+				}
+			}
+			else if(option == 3) {
+				break;
+			}
+		}
 	}
 }
